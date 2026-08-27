6016389c978208d58e3ba1a19b460f1ef430f61e
Asmit Chowdhury <asmit.chowdhury.2007@gmail.com>
2026-08-28 00:33:52 +0530

    Created Genesis Block

--- src/block.ts
+++ src/block.ts
@@ -1,4 +1,5 @@
-class block{
+import GENESIS_BLOCK from "./config.js"
+class Block{
     timestamps : string;
     prevHash : string;
     hash : string;
@@ -9,7 +10,17 @@ class block{
         this.hash = hash;
         this.data = data;
     }
+    static genesis() : Block{
+        return new Block(
+            GENESIS_BLOCK.timestamps,
+            GENESIS_BLOCK.prevHash,
+            GENESIS_BLOCK.hash,
+            GENESIS_BLOCK.data
+        )
+    }
 }
 
-const block1 = new block("27/8","ex567","qwer456","Hi Asmit");
+const block1 = new Block("27/8","ex567","qwer456","Hi Asmit");
+const genesis = Block.genesis();
+console.log(genesis);
 console.log(block1);
---
+++ src/config.ts
@@ -0,0 +1,8 @@
+const GENESIS_BLOCK = {
+    timestamps : "1",
+    prevHash : "00000x000",
+    hash : "asbcns34x",
+    data : "GENESIS"
+}
+
+export default GENESIS_BLOCK;
