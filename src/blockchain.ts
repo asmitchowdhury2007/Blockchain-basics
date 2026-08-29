f45aeb0487c0e0a144fcea502740f32076cefe38
Asmit Chowdhury <asmit.chowdhury.2007@gmail.com>
2026-08-29 19:51:39 +0530

    Minor Changes

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -48,31 +48,3 @@ const block1 = blockchain.addBlock("Block1");
 const block2 = blockchain.addBlock("Block2");
 const block3 = blockchain.addBlock("Block3");
 
-console.log(blockchain.chain)
-console.log(BlockChain.chain_validation(blockchain.chain));
-console.log(blockchain.longest_chain([
-  {
-    timestamps: '1',
-    prevHash: '00000x000',
-    hash: 'asbcns34x',
-    data: 'GENESIS'
-  },
-   {
-    timestamps: 1788011754451,
-    prevHash: 'asbcns34x',
-    hash: 'e0c69598ca398cf8ce4201cf6e15cfa71ea4ff0552bd7a5bb871539b5838809c',
-    data: 'Block1'
-  },
-  {
-    timestamps: 1788011754451,
-    prevHash: 'e0c69598ca398cf8ce4201cf6e15cfa71ea4ff0552bd7a5bb871539b5838809c',
-    hash: 'e6d3e3a0010fedf2d5f18f2c0afbc004c3c1b3797c3d4254c1194bd1ea0535a5',
-    data: 'Block2'
-  },
-  {
-    timestamps: 1788011754451,
-    prevHash: 'e6d3e3a0010fedf2d5f18f2c0afbc004c3c1b3797c3d4254c1194bd1ea0535a5',
-    hash: 'a652d7e0948e3befd3b0f6610e83b92d0be04c0a6972ced6878a3ba01fc53b57',
-    data: 'Block4'
-  }
-]))
